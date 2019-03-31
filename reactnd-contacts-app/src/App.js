8b900ee23531da1214ac54514e735da415c464fb
Luiz Gustavo <luizlgom@gmail.com>
2019-03-31 14:39:37 -0300

    +files

---
+++ reactnd-contacts-app/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+class App extends Component {
+  state = { contacts: [] }
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+
+    ContactsAPI.remove(contact)
+  }
+  render() {
+    return (
+      <div>
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
